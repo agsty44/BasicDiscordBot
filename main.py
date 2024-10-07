160b8c97d0562e859de3e05d75ff0efee14127d7
BuildTools <unconfigured@null.spigotmc.org>
2024-10-07 07:54:51 +0100

    more commands

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ f = open("help.txt")
 helpText = f.read()
 f.close()
 
+logbook = open("logs.txt", "a")
+
 # using the command api
 # the arguments in the command are:
 # "ctx" is the current channel
@@ -24,13 +26,26 @@ f.close()
 @bot.command()
 async def ban(ctx, member : discord.Member, reason): #get the member as the second argument
     await member.ban(reason = reason)
-    banMessage = "@" + str(member) + " " + "was banned with reason:" + " " + reason
+    banMessage = "@" + str(member) + " was banned with reason: " + reason
     await ctx.send(banMessage)
+    logbook.write("Time: " + str(datetime.time) + " User: " + str(member) + " Action: Ban")
 
 # timeout
 @bot.command()
 async def timeout(ctx, member : discord.Member, timeInHours):
     await member.timeout(datetime.timedelta(hours=int(timeInHours)))
+    timeoutMessage = "@" + str(member) + " was timed out for: " + str(timeInHours) + " hours"
+
+@bot.command()
+async def kick(ctx, member : discord.Member):
+    await member.kick()
+    kickMessage = "@" + str(member) + " was kicked"
+    await ctx.send(kickMessage)
+
+@bot.command()
+async def warn(ctx, member : discord.Member, reason):
+    warnMessage = "@" + str(member) + " has been warned for: " + reason
+    await ctx.send(warnMessage)
 
 # run the bot below here, dont modify
 
